af05f77a19e22d02c7529ec8fb4526c0a7b44521
Aaron Sumner <asumner@mac.com>
2013-06-18 22:47:43 -0500

    move controller out of index

---
+++ js/controllers.js
@@ -0,0 +1,30 @@
+function RosterController($scope) {
+  $scope.payroll = {};
+  $scope.newPet = {};
+
+  $scope.pets = [
+    { firstname: 'JoJo', species: 'Dog', salary: 50000 },
+    { firstname: 'Amber', species: 'Cat', salary: 70000 },
+    { firstname: 'Albert', species: 'Cat', salary: 100000 },
+    { firstname: 'Maggie', species: 'Cat', salary: 80000 },
+    { firstname: 'Yadier Molina', species: 'Cat', salary: 120000 },
+    { firstname: 'Hermie', species: 'Cat', salary: 55000 }
+  ];
+
+  $scope.addPet = function () {
+    $scope.pets.push({ firstname: $scope.newPet.name, species: $scope.newPet.species, salary: $scope.newPet.salary });
+    $scope.newPet = {};
+  };
+
+  $scope.deletePet = function(i) {
+    $scope.pets.splice(i,1);
+  };
+
+  $scope.$watch(function() {
+    var total = 0;
+    for (var i = 0; i < $scope.pets.length; i++) {
+      total = total + $scope.pets[i].salary * 1.0;
+    }
+    $scope.payroll.total = total;
+  });
+}
